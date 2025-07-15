ea600ed056c330181ecb06e08eeb42ea81645ee3
abhay-gits <anabhay42@gmail.com>
2025-07-16 00:01:09 +0530

    fix cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,12 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors(
-  {
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  }
-));
+app.use(cors());
 app.use(clerkMiddleware());
 app.use(express.json());
 
